b517809d03ee8f9b021431f09f4ea3892a9dc9e8
MEET <meetpansuriya93@gmail.com>
2024-10-13 18:11:48 +0530

    add switch

---
+++ switch.php
@@ -0,0 +1,46 @@
+<?php
+// Get the month number from user input
+$monthNumber = (int)readline("Enter a month number (1-12): ");
+
+// Use switch to display the corresponding month
+switch ($monthNumber) {
+    case 1:
+        echo "January";
+        break;
+    case 2:
+        echo "February";
+        break;
+    case 3:
+        echo "March";
+        break;
+    case 4:
+        echo "April";
+        break;
+    case 5:
+        echo "May";
+        break;
+    case 6:
+        echo "June";
+        break;
+    case 7:
+        echo "July";
+        break;
+    case 8:
+        echo "August";
+        break;
+    case 9:
+        echo "September";
+        break;
+    case 10:
+        echo "October";
+        break;
+    case 11:
+        echo "November";
+        break;
+    case 12:
+        echo "December";
+        break;
+    default:
+        echo "Invalid month number! Please enter a number between 1 and 12.";
+}
+?>
